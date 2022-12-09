e83aa4c28b3916d78e7b66d588686c9043618818
kiyo <kiyohiro.0310@gmail.com>
2022-12-09 23:34:22 +1100

    Do flexbox

--- App.js
+++ App.js
@@ -1,12 +1,63 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={{ padding: 50, flexDirection: "row", width: "80%", height: 300, justifyContent: 'space-between', alignItems: 'stretch' }}>
+      <View
+        style={{
+          backgroundColor: "red",
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>1</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "blue",
+          flex: 2,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "yellow",
+          flex: 3,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>3</Text>
+      </View>
+      {/* <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
+        <Button title="Add Goal"/>
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View> */}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-});
+// const styles = StyleSheet.create({
+//   appContainer: {
+//     padding: 50
+//   },
+//   inputContainer: {
+//     flexDirection: "row",
+//     justifyContent: "space-between"
+//   },
+//   textInput: {
+//     borderWidth: 1,
+//     borderColor: '#cccccc',
+//     width: '80%',
+//     marginRight: 8,
+//     padding: 8
+//   }
+// });
